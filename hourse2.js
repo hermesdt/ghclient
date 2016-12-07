5e3b64054ffe9e46986730449d8606938ec61e53
Álvaro Durán Tovar <hermesdt@gmail.com>
2016-12-07 23:34:41 +0100

    adds animations to the creation of the hourse elements in the JS

--- hourse2.js
+++ hourse2.js
@@ -1,13 +1,22 @@
-'use strict';
-
 var Hourse = function(){
 }
 
+Hourse.animations = ['animation1', 'animation2'];
+
 Hourse.addElement = function(){
+    // clone and add new element to the DOM
     var template = document.getElementById("hourse-template");
     var content = template.content;
     var container = document.getElementById("hourses-container")
     container.appendChild(document.importNode(content, true));
+
+    // add animation class to this new element
+    var child = container.children[container.children.length - 1];
+    var index = parseInt(Math.random() * Hourse.animations.length);
+    child.classList.add(Hourse.animations[index]);
+
+    // scroll to bottom
+    window.scrollTo(0,document.body.scrollHeight);
 }
 
 document.addEventListener("DOMContentLoaded", function(){
